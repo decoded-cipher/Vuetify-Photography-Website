eadf27c69db826510c2850dfec07e784f6aed1d2
Arjun Krishna <arjun12345krishna@gmail.com>
2020-06-30 08:21:10 +0530

    NotFound Page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,6 +1,12 @@
 <template>
     <v-content>
-        Not Found
+        <v-container>
+            <div class="block text-center notFound">
+                <h2>Page not Found</h2>
+                <i class="fas fa-exclamation-triangle red--text"></i>
+                <p>We're sorry, the page you requested could not be found. Please go back to the Home Page</p>
+            </div>
+        </v-container>
     </v-content>
 </template>
 
